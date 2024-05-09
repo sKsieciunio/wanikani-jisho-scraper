5726c12e4f314ad5c0e1da9fdaef13ae9201a0e8
Hubert Blonowski <135438393+sKsieciunio@users.noreply.github.com>
2024-05-09 11:24:17 +0200

    works neatly, added argv handling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
+import sys
+import argparse
 from typing import List
-from jisho_api import scrape
 from jisho_api.word import Word
 from rich.console import Console
 
@@ -19,45 +19,57 @@ class Vocab:
 		return return_string
 
 
-def parse_res_to_Vocab(res) -> List[Vocab]:
+def parse_res_to_Vocab(res, n) -> List[Vocab]:
 	Vocabs = []
-	for elem in res:
+	for index, elem in enumerate(res):
+		if index >= n:
+			break
 		definitions = [sense.get('english_definitions', []) for sense in elem.get("senses", [])]
 		Vocabs.append(Vocab(elem['slug'], definitions))
 	return Vocabs
 
+if __name__ == '__main__':
+	# parsing cli arguments
+	argv_parser = argparse.ArgumentParser(
+		prog='wanikani jisho parser',
+		description='It does something',
+		epilog='thats epilog'
+	)
+	argv_parser.add_argument('searchWord', type=str)
+	argv_parser.add_argument('-n', '--number', type=int)
+	args = argv_parser.parse_args()
 
+	# scraping data from jisho
+	res = Word.request(args.searchWord)
+	res = res.dict()
+	res = res['data']
 
-res = Word.request('water')
-res = res.dict()
-res = res['data']
+	# data cleanup
+	keys_to_remove = ['is_common',
+					'tags',
+					'jlpt',
+					'japanese',
+					'parts_of_speech',
+					'links',
+					'restrictions',
+					'see_also',
+					'antonyms',
+					'source',
+					'info']
 
-keys_to_remove = ['is_common',
-				  'tags',
-				  'jlpt',
-				  'japanese',
-				  'parts_of_speech',
-				  'links',
-				  'restrictions',
-				  'see_also',
-				  'antonyms',
-				  'source',
-				  'info']
-
-for word in res:
-	for key in keys_to_remove:
-		if key in word:
-			del word[key]
-	for sense in word['senses']:
+	for word in res:
 		for key in keys_to_remove:
-			if key in sense:
-				del sense[key]
+			if key in word:
+				del word[key]
+		for sense in word['senses']:
+			for key in keys_to_remove:
+				if key in sense:
+					del sense[key]
 
-print(json.dumps(res, indent=4))
+	# parsing cleanedup data to Vocab objects
+	dictionary = parse_res_to_Vocab(res, args.number)
 
-vocabs = parse_res_to_Vocab(res)
-
-console = Console()
-console.print(f"[red bold]=================LOGS==================")
-for elem in vocabs:
-	print(elem)
+	console = Console()
+	console.print(f"[red bold]=================Jisho==================")
+	for elem in dictionary:
+		print(elem)
